d25c961dac3a06a1fcfc752d68cf42ea281dee1c
[dyad] <vasfnegocios@gmail.com>
2025-11-01 19:20:41 -0300

    [dyad] Removendo o texto 'Fixar Menu' do botão de pin no menu lateral. - wrote 1 file(s)

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -233,7 +233,7 @@ const Layout: React.FC = () => {
                 title={isMenuManual ? 'Desafixar Menu (Modo Automático)' : 'Fixar Menu (Modo Manual)'}
               >
                 <Pin className={`h-5 w-5 ${isMenuManual ? 'text-red-600' : 'text-slate-400'}`} />
-                {isExpanded && <span className="ml-3 text-sm font-medium text-slate-700 dark:text-slate-300">Fixar Menu</span>}
+                {/* Texto removido */}
               </button>
               
               {/* Botão de Sair */}
